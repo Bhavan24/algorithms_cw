30aa6a0f29d0dd997585fbe2c4307aa48a52f7a5
bhavan <bhavan2410@gmail.com>
2022-03-20 22:46:32 +0530

    add pdf

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String game = ".....0...S\n" +
+                "....0.....\n" +
+                "0.....0..0\n" +
+                "...0....0.\n" +
+                ".F......0.\n" +
+                ".0........\n" +
+                ".......0..\n" +
+                ".0.0..0..0\n" +
+                "0.........\n" +
+                ".00.....0.\n";
+        System.out.println(game);
     }
 }
