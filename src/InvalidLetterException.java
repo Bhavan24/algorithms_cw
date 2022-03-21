34f08e1b15e15db99c3af416e34201c8a86bbeea
99x-dgCandidate-Loganathan-1642230000037-at-675482622082 <loganthan.20201212@iit.ac.lk>
2022-03-21 21:37:11 +0530

    implement base code with an example

---
+++ src/InvalidLetterException.java
@@ -0,0 +1,5 @@
+class InvalidLetterException extends Exception {
+    public InvalidLetterException(String paramString) {
+        super(paramString);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String data;
-        ReadInputData readFromFile = new ReadInputData();
-        data = readFromFile.readFromFile("src/test.txt");
-        System.out.println(data);
-        data = readFromFile.readFromCommandLine("Enter data: ");
-        System.out.println(data);
+
+        try {
+            String filename = "src/test.txt";
+
+            InputHandler handler = new InputHandler();
+            SquareGraph graph = handler.readMap(filename);
+
+            ArrayList<Node> path = graph.executeAStar();
+
+            if (path == null) {
+                System.out.println("There is no path to target");
+            } else {
+
+                graph.printPath(path);
+                System.out.println("Done!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
