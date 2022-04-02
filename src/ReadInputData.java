6ce11bd35901a653a9bee46fe1339009bd8197c4
99x-dgCandidate-Loganathan-1642230000037-at-675482622082 <loganthan.20201212@iit.ac.lk>
2022-04-03 01:03:54 +0530

    method to read data

--- src/ReadInputData.java
+++ src/ReadInputData.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadInputData {
@@ -41,4 +44,53 @@ public class ReadInputData {
         scanner.close();
         return text.toString();
     }
+
+
+    public String[][] readFromFile(String filePath, int length) {
+        String[][] graph = new String[length][length];
+        try (Scanner scanner = new Scanner(new FileReader(filePath))) {
+            int i = 0;
+            while (scanner.hasNext()) {
+                graph[i] = scanner.nextLine().split("");
+                i++;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return graph;
+    }
+
+
+    public void readAllStrings(String filePath) {
+        ArrayList<ArrayList<String>> maps = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new FileReader(filePath))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] smallMap = line.split("");
+                maps.add(new ArrayList<>(Arrays.asList(smallMap)));
+            }
+            System.out.println(maps);
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String basePath = "src/inputs/";
+        File[] files = new File(basePath).listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (file.isFile()) {
+                String fileName = file.getName();
+                int length = Integer.parseInt(fileName.split("_")[1].replace(".txt", ""));
+                String[][] result = new ReadInputData().readFromFile(basePath + fileName, length);
+
+            }
+        }
+    }
+
 }
