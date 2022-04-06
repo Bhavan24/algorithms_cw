d7a540f39e89741229aa3b686af5dabe8d36e600
bhavan <bhavanl@techprovint.com>
2022-04-07 01:39:56 +0530

    implement file parsing

--- src/real/PuzzleFileHandler.java
+++ src/real/PuzzleFileHandler.java
@@ -3,11 +3,13 @@ package real;
 import java.io.FileReader;
 import java.util.Scanner;
 
+import static real.GameConstants.*;
+
 public class PuzzleFileHandler {
     public PuzzleFileHandler() {
     }
 
-    public String initializeFileContents(String filePath) {
+    public String readPuzzleFile(String filePath) {
         String fileContents = "";
         try {
             Scanner reader = new Scanner(new FileReader(filePath));
@@ -17,11 +19,59 @@ public class PuzzleFileHandler {
             }
             reader.close();
             fileContents = stringBuilder.toString();
-            System.out.println("\nPUZZLE LOADED!\n");
+            System.out.println(PUZZLE_LOADED);
             System.out.println(fileContents);
         } catch (Exception e) {
-            System.out.println("\nFILE DOES NOT EXIST!\nTHANK YOU!");
+            System.out.println(FILE_DOES_NOT_EXIST);
+            System.out.println(THANK_YOU);
         }
         return fileContents;
     }
+
+
+    public PuzzleMap initializePuzzleMap(String fileContents) {
+
+        PuzzleMap puzzleMap = new PuzzleMap();
+
+        if (fileContents == null || (fileContents = fileContents.trim()).equals("")) {
+            System.out.println(FILE_IS_EMPTY);
+            return null;
+        }
+
+        String[] lines = fileContents.split("[\r]?\n");
+        int rows = lines.length;
+        int columns = lines[0].length();
+
+        System.out.println(rows + " X " + columns);
+
+
+        for (int i = 0; i < rows; i++) {
+            if (lines[i].length() != columns) {
+                System.out.println(INVALID_DATA);
+                return null;
+            } else {
+                for (int j = 0; j < columns; j++) {
+                    switch (lines[i].charAt(j)){
+                        case ICE:
+                            System.out.println("ICE");
+                            break;
+                        case ROCK:
+                            System.out.println("ROCK");
+                            break;
+                        case START:
+                            System.out.println("START");
+                            System.out.println(i + " " + j);
+                            break;
+                        case FINISH:
+                            System.out.println("FINISH");
+                            System.out.println(i + " " + j);
+                            break;
+                    }
+                }
+            }
+        }
+
+        return puzzleMap;
+    }
+
 }
--- src/real/SlidingPuzzleGame.java
+++ src/real/SlidingPuzzleGame.java
@@ -1,26 +1,21 @@
 package real;
 
-import java.util.Scanner;
+import static real.GameConstants.WELCOME_TEXT;
 
 public class SlidingPuzzleGame {
 
     public static void main(String[] args) {
 
         // prompt and read the file path
-        String gameText = (
-                "+ - - - - - - - - - - - - - - - - - - - - - - +\n" +
-                "|         WELCOME TO SLIDING PUZZLES          |\n" +
-                "+ - - - - - - - - - - - - - - - - - - - - - - +\n" +
-                "\nPLEASE ENTER THE PUZZLE FILE PATH: "
-        );
-        System.out.print(gameText);
+        System.out.print(WELCOME_TEXT);
 //        Scanner scanner = new Scanner(System.in);
 //        String puzzleFilePath = scanner.next();
         String puzzleFilePath = "src/test.txt";
 
         // read the file from using the file handler
         PuzzleFileHandler fileHandler = new PuzzleFileHandler();
-        String fileContents = fileHandler.initializeFileContents(puzzleFilePath);
+        String fileContents = fileHandler.readPuzzleFile(puzzleFilePath);
+        PuzzleMap puzzleMap = fileHandler.initializePuzzleMap(fileContents);
     }
 
 }
