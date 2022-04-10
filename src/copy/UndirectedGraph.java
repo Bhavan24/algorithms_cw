2f3618b33e1242326df60a7bdf6cf99ae7d5efa2
bhavan <bhavanl@techprovint.com>
2022-04-10 17:24:22 +0530

    add copy folder

---
+++ src/copy/UndirectedGraph.java
@@ -0,0 +1,71 @@
+package copy;
+
+import java.util.*;
+
+public class UndirectedGraph {
+    private Map<Vertex, List<Vertex>> adjVertices = new HashMap<>();
+
+    public UndirectedGraph() {}
+
+    public UndirectedGraph(Map<Vertex, List<Vertex>> adjVertices) {
+        this.adjVertices = adjVertices;
+    }
+
+    public Map<Vertex, List<Vertex>> getAdjVertices() {
+        return adjVertices;
+    }
+
+    public void setAdjVertices(Map<Vertex, List<Vertex>> adjVertices) {
+        this.adjVertices = adjVertices;
+    }
+
+    public void addVertex(int label) {
+        adjVertices.putIfAbsent(new Vertex(label), new ArrayList<>());
+    }
+
+    public void addEdge(int label1, int label2) {
+        Vertex v1 = new Vertex(label1);
+        Vertex v2 = new Vertex(label2);
+        adjVertices.get(v1).add(v2);
+        //adjVertices.get(v2).add(v1);
+    }
+
+    public List<Vertex> getAdjVertices(int label) {
+        return adjVertices.get(new Vertex(label));
+    }
+
+    public Map<Integer, Integer> breadthFirstTraversal(UndirectedGraph graph, int startId, int endId) {
+        Set<Integer> visited = new LinkedHashSet<Integer>();
+        Queue<Integer> queue = new LinkedList<Integer>();
+        Map<Integer, Integer> pastVertexMap = new HashMap<>();
+        queue.add(startId);
+        visited.add(startId);
+        pastVertexMap.put(startId, null);
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+            for (Vertex v : graph.getAdjVertices(vertex)) {
+                if (!visited.contains(v.getLabel())) {
+                    pastVertexMap.put(v.getLabel(), vertex);
+                    visited.add(v.getLabel());
+                    queue.add(v.getLabel());
+                }
+            }
+        }
+        return pastVertexMap;
+    }
+
+    public List<Integer> findPathList(Map<Integer, Integer> pastVertexMap, int startId, int endId) {
+        int newId = -1;
+        List<Integer> pathList = new ArrayList<>();
+        pathList.add(endId);
+        while (newId != startId) {
+            newId = pastVertexMap.get(endId);
+            pathList.add(newId);
+            endId = newId;
+        }
+        Collections.reverse(pathList);
+        return pathList;
+    }
+
+}
+
