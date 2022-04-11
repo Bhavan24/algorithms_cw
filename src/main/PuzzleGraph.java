ad10a35cba69abe6e353715e3ea984708125e761
bhavan <bhavanl@techprovint.com>
2022-04-12 03:45:01 +0530

    add graph data structure

--- src/main/PuzzleGraph.java
+++ src/main/PuzzleGraph.java
@@ -28,21 +28,48 @@ import java.util.Map;
 
 public class PuzzleGraph {
 
-    private final Map<PuzzleVertex, List<PuzzleVertex>> adjVertices = new HashMap<>();
+    private final Map<PuzzleVertex, List<PuzzleVertex>> adjVertices;
 
     public PuzzleGraph() {
+        this.adjVertices = new HashMap<>();
     }
 
-    public void addVertex(int id) {
-        adjVertices.putIfAbsent(new PuzzleVertex(id), new ArrayList<>());
+    public void addVertex(int vertexId) {
+        adjVertices.putIfAbsent(new PuzzleVertex(vertexId), new ArrayList<>());
     }
 
-    public void addEdge(int id1, int id2) {
-        adjVertices.get(new PuzzleVertex(id1)).add(new PuzzleVertex(id2));
+    public void removeVertex(int vertexId) {
+        PuzzleVertex v = new PuzzleVertex(vertexId);
+        adjVertices.values().forEach(e -> e.remove(v));
+        adjVertices.remove(new PuzzleVertex(vertexId));
     }
 
-    public List<PuzzleVertex> getAdjVertices(int label) {
-        return adjVertices.get(new PuzzleVertex(label));
+    public void addEdge(int vertexId1, int vertexId2) {
+        PuzzleVertex v1 = new PuzzleVertex(vertexId1);
+        PuzzleVertex v2 = new PuzzleVertex(vertexId2);
+        adjVertices.get(v1).add(v2);
     }
 
+    public void removeEdge(int vertexId1, int vertexId2) {
+        PuzzleVertex v1 = new PuzzleVertex(vertexId1);
+        PuzzleVertex v2 = new PuzzleVertex(vertexId2);
+        List<PuzzleVertex> eV1 = adjVertices.get(v1);
+        List<PuzzleVertex> eV2 = adjVertices.get(v2);
+        if (eV1 != null) eV1.remove(v2);
+        if (eV2 != null) eV2.remove(v1);
+    }
+
+    public List<PuzzleVertex> getAdjVertices(int vertexId) {
+        return adjVertices.get(new PuzzleVertex(vertexId));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (PuzzleVertex v : adjVertices.keySet()) {
+            sb.append(v);
+            sb.append(adjVertices.get(v));
+        }
+        return sb.toString();
+    }
 }
