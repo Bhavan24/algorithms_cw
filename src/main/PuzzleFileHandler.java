5795ab777d8892cacf30b65ddfa49878a6daeefb
bhavan <bhavanl@techprovint.com>
2022-04-12 02:50:04 +0530

    add comments

--- src/main/PuzzleFileHandler.java
+++ src/main/PuzzleFileHandler.java
@@ -20,15 +20,46 @@ import java.util.Scanner;
 
 import static main.PuzzleConstants.*;
 
+/**
+ * <p>The {@code PuzzleFileHandler} reads text from the file path using the
+ * {@linkplain Scanner} object.</p>
+ * <p>
+ * The file will be read and converted to a string.
+ * If the file reading gets failed, the user will be prompted a message.
+ *
+ * @author Loganathan Bhavaneetharan
+ */
+
 public class PuzzleFileHandler {
 
+    /**
+     * File path value
+     */
     private final String filePath;
+
+    /**
+     * All the contents of the file will be stored in this String
+     */
     private String fileContents;
 
+    /**
+     * Constructs and initializes a PuzzleFileHandler.
+     *
+     * @param filePath the path of the file to read
+     */
     public PuzzleFileHandler(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Reads the puzzle file and stores all the content as String
+     * and finally returns the string.
+     *
+     * The exceptions are handled by using a try catch, user will be prompted a
+     * message if an exception occurred
+     *
+     * @return the file contents of the given file
+     */
     public String readPuzzleFile() {
         try {
             Scanner reader = new Scanner(new FileReader(filePath));
--- src/main/Test.java
+++ src/main/Test.java
@@ -1,11 +1,14 @@
 package main;
 
 import java.awt.*;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
 //        System;
 //        String;
 //        Point
+//        Scanner
     }
 }
