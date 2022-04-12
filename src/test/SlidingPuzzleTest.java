b336447bf61008f0425dfbf22c2e340dffb6d5aa
bhavan <bhavanl@techprovint.com>
2022-04-12 21:36:13 +0530

    fix unit test

---
+++ src/test/SlidingPuzzleTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import main.PuzzleConstants.IceState;
+import main.SlidingPuzzle;
+
+public class SlidingPuzzleTest {
+
+    private static final int directionType = 1;
+    private static final int iceStateType = 2;
+    private static final boolean runAllFiles = true;
+    private static final int fileIndex = 10;
+
+    public static void main(String[] args) {
+        SlidingPuzzle slidingPuzzle = new SlidingPuzzle();
+        String[] files = slidingPuzzle.loadPuzzleFiles();
+
+        if (runAllFiles) {
+            for (String file : files) {
+                runSlidingPuzzle(slidingPuzzle, directionType, iceStateType, file);
+            }
+        } else {
+            String file = files[fileIndex];
+            runSlidingPuzzle(slidingPuzzle, directionType, iceStateType, file);
+        }
+    }
+
+    private static void runSlidingPuzzle(SlidingPuzzle slidingPuzzle, int directionType, int iceStateType, String file) {
+        try {
+            int[][] directions = slidingPuzzle.selectDirectionType(directionType);
+            IceState iceState = slidingPuzzle.selectIceStateType(iceStateType);
+            slidingPuzzle.solvePuzzle(file, directions, iceState);
+            System.out.println(file + " completed running");
+        } catch (Exception e) {
+            System.out.println("An error occurred in file " + file + " " + e.getMessage());
+        }
+    }
+
+}
